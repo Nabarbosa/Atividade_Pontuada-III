c470d5b629fcc8ea10fdb79e0c7f4283c0521707
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-06-03 10:22:23 -0300

    Delete src directory

--- src/main/java/com/example/aula/exception/PratoJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class PratoJaCadastradoException extends RuntimeException {
-    public PratoJaCadastradoException(String message) {
-        super(message);
-    }
-}
